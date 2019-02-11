9c5629d80f98b3095085f693c3dc2d49f684bbb7
JCmusik <jcmusik@gmail.com>
2019-02-11 14:45:43 -0600

    used bable and jsx syntax

--- inClass/src/js/main.js
+++ inClass/src/js/main.js
@@ -1,4 +1,24 @@
 let root = document.getElementById("root");
-let content = React.createElement("h1", {}, "Hello World");
 
-ReactDOM.render(content, root);
+let content = React.createElement(
+  "ul",
+  {},
+  React.createElement("li", {}, "First Item"),
+  React.createElement("li", {}, "Second Item")
+);
+
+let jsxcontent = (
+  <ul>
+    <li>
+      <span className="text-danger">First Item</span>
+    </li>
+    <li>
+      <span className="text-warning">Second Item</span>
+    </li>
+    <li>
+      <span className="text-success">Third Item</span>
+    </li>
+  </ul>
+);
+
+ReactDOM.render(jsxcontent, root);
