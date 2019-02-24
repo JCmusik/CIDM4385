fa870c3e787f5265c9d9ff494e74bc8be1a3c0cf
JCmusik <jcmusik@gmail.com>
2019-02-24 15:30:04 -0600

    added firebase to project

---
+++ assignment3/src/app.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+    state = {}
+    render() {
+        return (
+            <h1>Beginning</h1>
+        );
+    }
+}
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root)
