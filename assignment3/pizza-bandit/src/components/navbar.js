5085a16356b62fd46e7fd2cbabc7b02632911d15
JCmusik <jcmusik@gmail.com>
2019-03-03 09:36:55 -0600

    updated navbar app name

--- assignment3/pizza-bandit/src/components/navbar.js
+++ assignment3/pizza-bandit/src/components/navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div className="header bg-secondary text-white text-center">
-            Low Budget Pizza Delivery
+            Pizza Bandit
         </div>
     )
 }
