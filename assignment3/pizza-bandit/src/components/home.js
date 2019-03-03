f97d826e19da1e979605985e001d62ab8895df86
JCmusik <jcmusik@gmail.com>
2019-03-03 09:24:33 -0600

    added bootstrap, jquery, popper

---
+++ assignment3/pizza-bandit/src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    state = {}
+    render() {
+        return (
+            <div className="container text-white">
+                <h1>Home Page</h1>
+            </div>);
+    }
+}
+
+export default Home;
