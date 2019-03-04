9e883e0dc36f4a0ad802a933bd30eb8e8cabed6d
JCmusik <jcmusik@gmail.com>
2019-03-03 22:01:32 -0600

    renamed navbar component to header

--- assignment3/pizza-bandit/src/App.js
+++ assignment3/pizza-bandit/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './components/home';
-import Navbar from './components/navbar';
+import Header from './components/navbar';
 import Footer from './components/footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Header />
         <Home />
         <Footer />
       </div>
--- assignment3/pizza-bandit/src/components/header.js
+++ assignment3/pizza-bandit/src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Navbar = () => {
+const Header = () => {
     return (
         <div className="header bg-secondary text-white text-center">
             Pizza Bandit
@@ -8,4 +8,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+export default Header;
